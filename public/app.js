fda5965c87833a7af1105b8cc0dbb693c98838b4
Gloria <gloriacheung812@gmail.com>
2022-07-01 01:25:45 +0000

    change to each show being article with title and rating

--- public/app.js
+++ public/app.js
@@ -1,28 +1,45 @@
 const searchForm = document.querySelector("#searchForm");
-const container = document.querySelector("ul.container");
+const container = document.querySelector(".container");
 
+// helper functions
 const resetInput = function() {
   searchForm.elements.query.value = '';
 };
-const createImage = function(results) {
-  for (let show of results.data) {
-    if (show.show.image.medium) {
+
+const createShow = function(resultsArray) {
+  for (let show of resultsArray) {
+    if (show.show.image) {
       const img = document.createElement("img");
       img.src = show.show.image.medium;
-      const newShow = document.createElement("li");
+      const newShow = document.createElement("article");
+      const span = document.createElement("span");
+      if (show.show.rating.average) {
+        span.innerText = `${show.show.name} - rated ${show.show.rating.average}`;
+      } else {
+        span.innerText = `${show.show.name}`;
+      }
+      
       newShow.append(img);
+      newShow.append(span);
       container.append(newShow); 
     }
   }
 };
 
+
+const clearResults = function() {
+  container.innerHTML = "";
+};
+
+// event listener
 searchForm.addEventListener("submit", function(e) {
   e.preventDefault();
 
+  clearResults();
   const query = this.elements.query.value;
   axios.get(`https://api.tvmaze.com/search/shows?q=${query}`)
     .then((results) => {
-      createImage(results);
+      createShow(results.data);
       resetInput();
     })
     .catch((e) => {
