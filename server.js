2be469b3ae92b91f5661b493e50bbd7f60f15e54
Gloria <gloriacheung812@gmail.com>
2022-07-01 00:40:20 +0000

    setup server and instal packages

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const path = require("path");
+app.use(express.static("public")); 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+app.listen(PORT, () => {
+  console.log("listening on PORT 8080...");
+});
